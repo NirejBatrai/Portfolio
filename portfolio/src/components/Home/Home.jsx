25bdc60c7d247beea424c8a5e27fb5406c725eb2
Nil <95134227+NirejBatrai@users.noreply.github.com>
2024-02-26 20:07:03 +0700

    added welcoming text

--- portfolio/src/components/Home/Home.jsx
+++ portfolio/src/components/Home/Home.jsx
@@ -1,7 +1,43 @@
 import React from "react";
+import "./Home.css";
+import { useNavigate } from "react-router-dom";
+import { Animate } from "react-simple-animate";
 
 function Home() {
-  return <div>Home Page</div>;
+  const navigate = useNavigate();
+
+  const handleNavigateToContactMePage = () => {
+    navigate("/contact");
+  };
+  return (
+    <section id='home' className='home'>
+      <div className='home_text-wrapper'>
+        <h1>
+          Hello, I am Nirej
+          <br />
+          Front end developer
+        </h1>
+      </div>
+
+      <Animate
+        play
+        duration={1.5}
+        delay={1}
+        start={{
+          transform: "translateY(550px)",
+        }}
+        end={{
+          transform: "translateY(0px)",
+        }}
+      >
+        <div className='home_contact-me'>
+          <button onClick={handleNavigateToContactMePage}>
+            Hire me please
+          </button>
+        </div>
+      </Animate>
+    </section>
+  );
 }
 
 export default Home;
