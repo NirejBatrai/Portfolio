ad2aebb4be4773d9d580a41b7f05a4c711b8a1ff
Nil <95134227+NirejBatrai@users.noreply.github.com>
2024-02-28 15:33:47 +0700

    added data for skillsdata

---
+++ portfolio/src/components/skills/utils.js
@@ -0,0 +1,47 @@
+export const skillsData = [
+  {
+    label: "FRONT END",
+    data: [
+      {
+        skillName: "HTML",
+        percentage: "90",
+      },
+      {
+        skillName: "CSS",
+        percentage: "59",
+      },
+      {
+        skillName: "JAVASCRIPT",
+        percentage: "78",
+      },
+    ],
+  },
+
+  {
+    label: "BACK END",
+    data: [
+      {
+        skillName: "NODE JS",
+        percentage: "50",
+      },
+      {
+        skillName: "C#",
+        percentage: "10",
+      },
+    ],
+  },
+
+  {
+    label: "DATA BASE",
+    data: [
+      {
+        skillName: "MYSQL",
+        percentage: "50",
+      },
+      {
+        skillName: "MONGODB",
+        percentage: "20",
+      },
+    ],
+  },
+];
