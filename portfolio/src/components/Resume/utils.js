16b3c04c22ef5cc527a90ef26e070c56f60b97c4
Nil <95134227+NirejBatrai@users.noreply.github.com>
2024-02-28 15:33:28 +0700

    added utils to save data of edu and expre

---
+++ portfolio/src/components/Resume/utils.js
@@ -0,0 +1,32 @@
+export const data = {
+  experience: [
+    {
+      title: "Self-Study and Website Development",
+      subTitle: "Self-Employed",
+      description: "Self-learning web development.",
+    },
+    {
+      title: "University Studies",
+      subTitle: "Student",
+      description: "PSU student, Online-Studying.",
+    },
+  ],
+
+  education: [
+    {
+      title: "High School",
+      subTitle: "GED",
+      description: "12th Grade",
+    },
+    {
+      title: "Secondary School",
+      subTitle: "Phuket Pals",
+      description: "10th Grade",
+    },
+    {
+      title: "University",
+      subTitle: "Prince of Songkla University",
+      description: "Currently Studying",
+    },
+  ],
+};
